ff3c64b13cfb559c83ea23998e1e1fb46c04c37a
Your Name <you@example.com>
2022-09-29 01:03:34 +0600

    added Card section

--- src/component/Card/Card.js
+++ src/component/Card/Card.js
@@ -10,26 +10,29 @@ const Card = () => {
   },[])
 
   return (
-    <div>
+    <div className='mt-5'>
       <h2 className='ml-4 text-xl'>Select today’s exercise</h2> 
-     <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3'>
-     {
-        cards.map(card => <SingleCard></SingleCard>)
+     <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mt-10 gap-4'>
+      {
+        cards.map(card => <SingleCard key={card.id} card={card}></SingleCard>)
       }
      </div>
     </div>
   );
 };
 
-const SingleCard = () => {
+const SingleCard = (props) => {
+  // console.log(props);
+  const{age ,img,time,name} = props.card
   return(
-    <div className="card card-compact w-96 bg-base-100 shadow-xl">
-  <figure><img src="https://placeimg.com/400/225/arch" alt="Shoes" /></figure>
+    <div className="card card-compact w-96 bg-base-100 shadow-xl w-full">
+  <figure><img className='w-full rounded h-56' src={img} alt="Shoes" /></figure>
   <div className="card-body">
-    <h2 className="card-title">Shoes!</h2>
-    <p>If a dog chews shoes whose shoes does he choose?</p>
+    <h2 className="card-title">{name}</h2>
+    <p>For Age : {age}</p>
+    <p>Time required : <span>{time}</span>s</p>
     <div className="card-actions justify-end">
-      <button className="btn btn-primary">Buy Now</button>
+      <button className="btn btn-primary btn-sm w-full">Add To List</button>
     </div>
   </div>
 </div>
